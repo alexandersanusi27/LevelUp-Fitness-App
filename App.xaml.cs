c31025772ec23de884285abdb7f3b60678bea031
Alexander Sanusi <alexandersanusi27@gmail.com>
2026-04-16 03:11:15 +0100

    add App class and load saved state on startup

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LevelUp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+		AppState.Load(); // restore XP and quest state; auto-resets quests if it's a new day
+
+		// schedule the 8pm daily reminder - fire and forget, no need to await here
+		_ = NotificationService.ScheduleDailyReminderAsync();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
